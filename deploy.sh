c6386b77dd40cfdb0f5d274714f506c92839dba7
edouae <128542991+edouae@users.noreply.github.com>
2024-12-22 07:59:48 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Variables (Customize these)
 DOCKER_USERNAME="douae196"
-DOCKER_PASSWORD="dckr_pat_iiHc5ZGWd7u-r5-M9w5FXUA8vhI"
+DOCKER_PASSWORD="dckr_pat_m8CEo1aa4YqFCb6mXdmfAj_0Lqk"
 DOCKER_IMAGE_NAME="douaeimage"
 DOCKER_IMAGE_TAG="latest"
 REMOTE_SERVER="192.168.19.130"
